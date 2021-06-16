e2fa1d0ff078d99bd41454a9e1da9943826e807f
Varinder148 <34982959+Varinder148@users.noreply.github.com>
2021-06-16 16:29:23 +0530

    added counter

--- src/redux/game/game.utils.js
+++ src/redux/game/game.utils.js
@@ -7,3 +7,51 @@ export const filterPlanets = (origPlanets, playerPlanetsObject) => {
   });
   return filteredRes;
 };
+
+export const disableVehicles = (
+  selectorId,
+  planetBeingSelected,
+  filteredVehicles,
+  origVehicles,
+  availableVehicleCount
+) => {
+  let VehiclesThatCanReachThisPlanet = origVehicles.map((vehicle) => {
+    let isDisabled = vehicle.max_distance <= planetBeingSelected.distance;
+
+    let vehicleCountFound = availableVehicleCount.find(
+      (countVehicle) => countVehicle.name === vehicle.name
+    );
+
+    isDisabled = isDisabled || vehicleCountFound <= 0;
+
+    return { ...vehicle, disabled: isDisabled };
+  });
+
+  console.log("res", VehiclesThatCanReachThisPlanet);
+  return { ...filteredVehicles, [selectorId]: VehiclesThatCanReachThisPlanet };
+};
+
+export const initiateCount = (vehicles) => {
+  return vehicles.map((vehicle) => ({
+    name: vehicle.name,
+    total_no: vehicle.total_no,
+  }));
+};
+
+export const decrement = (playerSelectedVehicles, origVehicles) => {
+  let res = [];
+
+  playerSelectedVehicles = Object.values(playerSelectedVehicles);
+
+  origVehicles.forEach((origVehicle) => {
+    let vehiclesFound = playerSelectedVehicles.filter(
+      (vehicle) => origVehicle.name === vehicle.name
+    );
+    let count = origVehicle.total_no - vehiclesFound.length;
+    count = count <= 0 ? 0 : count;
+    let vehicle = { name: origVehicle.name, total_no: count };
+    res.push(vehicle);
+  });
+
+  return res;
+};
